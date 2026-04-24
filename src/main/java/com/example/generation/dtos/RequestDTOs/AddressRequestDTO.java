c216f87a0fd1a68b0835a096d925e16bc16fb979
Nana Yaa Osei-Amoako <730765@student.inholland.nl>
2026-04-24 21:55:11 +0200

    DTOs
    separated into requests and responses, only requests have validation

---
+++ src/main/java/com/example/generation/dtos/RequestDTOs/AddressRequestDTO.java
@@ -0,0 +1,22 @@
+package com.example.generation.dtos.RequestDTOs;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class AddressRequestDTO {
+    @NotBlank
+    @Size(min = 1, max = 128)
+    private String addressLine;
+    @NotBlank
+    @Size(min = 4, max = 10, message = "Postal code length is invalid")
+    private String postalCode;
+    @NotBlank
+    @Size(min = 1, max = 60)
+    private String city;
+    @NotBlank
+    @Size(min = 1, max = 60)
+    private String country;
+}
