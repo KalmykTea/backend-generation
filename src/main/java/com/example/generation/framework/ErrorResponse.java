a9c3d13e476547c049b50274d14009336b62450d
Nana Yaa Osei-Amoako <730765@student.inholland.nl>
2026-04-28 14:29:13 +0200

    Update ErrorResponse.java

--- src/main/java/com/example/generation/framework/ErrorResponse.java
+++ src/main/java/com/example/generation/framework/ErrorResponse.java
@@ -1,6 +1,7 @@
 package com.example.generation.framework;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
 import lombok.NonNull;
 
 import java.time.LocalDateTime;
@@ -8,13 +9,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+@Getter
 public class ErrorResponse {
     private int status;
     private String message;
     private LocalDateTime timestamp;
     private List<Map<String, String>> errors;
 
-    // Constructor
     public ErrorResponse(int status, String message, List<Map<String, String>> errors) {
         this.status = status;
         this.message = message;
