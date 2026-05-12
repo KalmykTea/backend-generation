f694c42d22b3d8a4d0cb67d5dcc0fe2e8faf411e
Nana Yaa Osei-Amoako <730765@student.inholland.nl>
2026-05-12 22:10:19 +0200

    Update AuthController.java
    added auth/me endpoint

--- src/main/java/com/example/generation/controllers/AuthController.java
+++ src/main/java/com/example/generation/controllers/AuthController.java
@@ -1,7 +1,10 @@
 package com.example.generation.controllers;
 
 import com.example.generation.dtos.RequestDTOs.LoginRequestDTO;
+import com.example.generation.dtos.ResponseDTOs.UserResponseDTO;
 import com.example.generation.entities.Account;
+import com.example.generation.entities.User;
+import com.example.generation.mappers.ResponseDTOMappers.UserResponseDTOMapper;
 import com.example.generation.services.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -9,17 +12,22 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @Tag(name = "Authorization", description = "Authorization related operations")
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
     final private AuthService authService;
+    private final UserResponseDTOMapper userResponseDTOMapper;
 
-    public AuthController(AuthService authService) {
+    public AuthController(AuthService authService,  UserResponseDTOMapper userResponseDTOMapper) {
         this.authService = authService;
+        this.userResponseDTOMapper = userResponseDTOMapper;
     }
 
     @PostMapping("/login")
@@ -39,5 +47,18 @@ public class AuthController {
     public ResponseEntity<String> login(@RequestBody LoginRequestDTO loginRequestDto) {
         String token = authService.login(loginRequestDto);
         return ResponseEntity.ok(token);
+    }
+
+    @GetMapping("/me")
+    UserResponseDTO me() {
+        Authentication authentication = org.springframework.security.core.context.SecurityContextHolder
+                .getContext()
+                .getAuthentication();
+
+        if (authentication == null || !(authentication.getPrincipal() instanceof User user)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Not authenticated");
+        }
+
+        return this.userResponseDTOMapper.toDTO(user);
     }
 }
