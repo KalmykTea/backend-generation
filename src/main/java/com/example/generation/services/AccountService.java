3acd8e66d2dc9a2331952097d33b14ff02ba35b1
Itzalem <729185@student.inholland.nl>
2026-06-06 10:02:50 -0400

    roll back changes in getIbanbyUsername

--- src/main/java/com/example/generation/services/AccountService.java
+++ src/main/java/com/example/generation/services/AccountService.java
@@ -85,11 +85,11 @@ public class AccountService {
         return accountFullResponseDTOMapper.toDTO(account);
     }
 
-    public List<AccountFullResponseDTO> getIbansByUserName(String firstName, String lastName) {
+    public List<String> getIbansByUserName(String firstName, String lastName) {
         return accountRepository
                 .findByUserFirstNameAndUserLastName(firstName, lastName)
                 .stream()
-                .map(accountFullResponseDTOMapper::toDTO)
+                .map(Account::getIban)
                 .toList();
     }
 
